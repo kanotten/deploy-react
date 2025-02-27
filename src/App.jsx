82f6ae5a9420474544a0f4926c40fd2ae589515a
Sheikh <kenneth.sheikh@hotmail.com>
2025-02-27 09:57:00 +0100

    added links to test page

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
+import { Link } from "react-router-dom";
+
 function App() {
   return (
     <>
-      <h1>EEEEEYOOOO</h1>
+      <h1>EEEEEYOOOO it works</h1>
+      <Link to="/test">To test page</Link>
+      <br />
+      <a href="/test">Anchor to test page</a>
     </>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,22 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import "./index.css";
+import App from "./App.jsx";
 
-createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/test",
+    element: <h1>Test path</h1>,
+  },
+]);
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
-)
+);
